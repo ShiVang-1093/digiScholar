f378c2c6a285937153bd20d52a714f8a0a87b32e
Shubham Sareliya <sareliashubham9999@gmail.com>
2023-06-04 12:38:50 +0530

    Resource Module Created

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     authController: require('./auth.controller'),
+    resourceController: require('./resource.controller'),
     newsController: require('./news.controller'),
     testimonialController: require('./testimonial.controller'),
 };
