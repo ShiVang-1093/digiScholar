021c873cef5b5e2c392fd21c275dcfc79fdbc1c6
ManavJoshi <manavjoshi154@gmail.com>
2023-06-08 09:33:07 +0530

    Home page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
-import Navbar from './Component/Navbar';
+import HomePage from './HomePage';
+// import Navbar from './Component/Navbar';
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/signup" element={<SignupPage/>} />
-        </Routes> 
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </Router>
   );
 };
 
 export default App;
 
-  
--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./LoginPage.css";
 
 function LoginPage() {
@@ -17,68 +17,75 @@ function LoginPage() {
     }));
   };
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    if (formData.email === '' || formData.password === '') {
-      alert('Please fill in both email and password fields.');
-      return;
-    }
 
-    if (!validatePassword()) {
-      alert(
-        'Invalid password. Please make sure it has at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one symbol.'
-      );
-      return;
-    }
-    
     console.log('Email:', formData.email);
     console.log('Password:', formData.password);
     setFormData({
       email: '',
       password: '',
     });
-  };
 
-  const validatePassword = () => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$/;
-    return passwordRegex.test(formData.password);
+    const res = await fetch("http://localhost:5000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password
+      })
+    });
+    const resJson = await res.json();
+    console.log(resJson);
+    localStorage.setItem("jwt", resJson.token);
+    if (res.status == 400 || res.status == 500 || !res) {
+      window.alert("Invalid Credentials");
+      console.log("Invalid Credentials");
+    }
+    else if (res.status == 200) {
+      window.alert("Login Successful");
+      console.log("Login Successful");
+    }
   };
 
   return (
     <div className="main">
       <div className="flex1">
         <div className="text">First step towards your dream Scholarship</div>
-      <img className="img" src="./Images/Graduation 2.png" alt="Error" srcset="" />
+        <img className="img" src="./Images/Graduation 2.png" alt="Error" srcset="" />
       </div>
       <div className="flex2">
         <div className="heading">Login To Your Account</div>
         <div className="form1">
-        <form>
-          <label>
-            <div className="mail2">Email</div>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange} 
-              required placeholder="abc@gmail.com"
-              value={formData.email}
-            />
-          </label >
-          <br />
-          <label>
-          <div className="pwd2">Password</div>
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              required placeholder="8 Charactor required"
-              value={formData.password}
-            />
-          </label>
-          <br />
-          <button type="submit" name="Login"onClick={handleLogin}>Login</button>
-        </form>
-        <div className="create">Don't have an account ? <Link to="/signup">Create Account</Link></div>
+          <form>
+            <label>
+              <div className="mail2">Email</div>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                required placeholder="abc@gmail.com"
+                value={formData.email}
+              />
+            </label >
+            <br />
+            <label>
+              <div className="pwd2">Password</div>
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                required placeholder="8 Charactor required"
+                value={formData.password}
+              />
+            </label>
+            <br />
+            <button type="submit" name="Login" onClick={handleLogin}>Login</button>
+          </form>
+          <div className="create">Don't have an account ? <Link to="/signup">Create Account</Link></div>
         </div>
       </div>
     </div>
