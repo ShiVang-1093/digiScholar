d5123e41c9c483f8a89047c7bc0615003524eda9
ManavJoshi <manavjoshi154@gmail.com>
2023-06-10 07:45:58 +0530

    Navbar added with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 import HomePage from './HomePage';
@@ -11,14 +12,14 @@ import Faq from './components/FAQ page/FAQs';
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="details" element={<Details />} />
-        <Route path="testinomial" element={<Testinomial />} />
-        <Route path="faq" element={<Faq />} />
-
+        <Route path="/details" element={<Details />} />
+        <Route path="/testimonial" element={<Testinomial />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from "./DigiScholar.svg";
+import usericon from "./usericon.svg";
+
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+                <div className="container-fluid shadow-lg mt-0">
+                    <NavLink to="index.html">
+                        <img
+                            src={Logo}
+                            className="logo"
+                            height="43px"
+                            width="242px"
+                            alt=""
+                        />
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    {/* <div className="d-flex justify-content-center"> */}
+                    <div
+                        className="collapse navbar-collapse items"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item ms-1">
+                                <NavLink
+                                    className="nav-link active-here"
+                                    aria-current="page"
+                                    to="/"
+                                >
+                                    Home
+                                </NavLink>
+                                <div className="active-div"></div>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/scholarships">
+                                    Scholarship
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/blogs">
+                                    Blogs
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/news">
+                                    News
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/testimonial">
+                                    Testimonials
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/faq">
+                                    FAQs
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/login">
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className="nav-item ms-3">
+                                <NavLink className="nav-link" to="/signup">
+                                    Signup
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <NavLink to="#">
+                            <img
+                                src={usericon}
+                                className="usericon"
+                                height="30px"
+                                width="36px"
+                                alt=""
+                            />
+                        </NavLink>
+                    </div>
+                    {/* </div> */}
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
