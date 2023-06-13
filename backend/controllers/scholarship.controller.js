edd09a5448872521488ac0323a13868cb5dc0fd5
Shubham Sareliya <sareliashubham9999@gmail.com>
2023-06-13 12:46:09 +0530

    Scholarship API Updated: Image Field Added

--- backend/controllers/scholarship.controller.js
+++ backend/controllers/scholarship.controller.js
@@ -1,9 +1,23 @@
+const cloudinary = require('cloudinary').v2;
+const { cloudinaryConfig } = require('../config');
 const { Scholarship } = require('../models');
+
+cloudinary.config(cloudinaryConfig);
+
+const folder = process.env.CLOUDINARY_FOLDER;
 
 exports.createScholarship = async (req, res) => {
     try {
-        req.body.created_by = req.user.id;
-        const scholarship = await Scholarship.create(req.body);
+        const { image } = req.files;
+
+        const imageResult = image[0].path;
+
+        const scholarship = await Scholarship.create({
+            ...req.body,
+            image: imageResult,
+            created_by: req.user.id
+        });
+
         return res.status(201).json({ scholarship });
     } catch (error) {
         return res.status(500).json({ error });
@@ -30,12 +44,39 @@ exports.getScholarship = async (req, res) => {
 
 exports.updateScholarship = async (req, res) => {
     try {
-        const scholarship = await Scholarship.findByIdAndUpdate(
+        const { image } = req.files;
+
+        const imageResult = image[0].path;
+
+        const scholarship = await Scholarship.findById(req.params.id);
+
+        if (scholarship.image !== imageResult) {
+            const imagePublicId = cloudinary
+                .url(scholarship.image)
+                .split('/')
+                .pop()
+                .split('.')[0];
+            await cloudinary.uploader.destroy(
+                `${folder}/${imagePublicId}`,
+                (error, result) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                    console.log(result);
+                }
+            );
+        }
+
+        const updatedScholarship = await Scholarship.findByIdAndUpdate(
             req.params.id,
-            req.body,
+            {
+                ...req.body,
+                image: imageResult
+            },
             { new: true }
         );
-        return res.status(200).json({ scholarship });
+
+        return res.status(200).json({ scholarship: updatedScholarship });
     } catch (error) {
         return res.status(500).json({ error });
     }
@@ -43,8 +84,26 @@ exports.updateScholarship = async (req, res) => {
 
 exports.deleteScholarship = async (req, res) => {
     try {
-        const scholarship = await Scholarship.findByIdAndDelete(req.params.id);
-        return res.status(200).json({ scholarship });
+        const test = await Scholarship.findByIdAndDelete(req.params.id);
+        if (!test) {
+            return res.status(404).json({ error: 'Scholarship not found' });
+        }
+        const scholarship = await Scholarship.findById(req.params.id);
+        const imagePublicId = cloudinary
+            .url(scholarship.image)
+            .split('/')
+            .pop()
+            .split('.')[0];
+        await cloudinary.uploader.destroy(
+            `${folder}/${imagePublicId}`,
+            (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                console.log(result);
+            }
+        );
+        return res.status(200).json({ message: 'Scholarship deleted' });
     } catch (error) {
         return res.status(500).json({ error });
     }
